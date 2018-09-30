dd23e5550702bb0b8a1d90292ac0d39aba0aacec
jr-garcia <jr-garcia@users.noreply.github.com>
2018-09-29 22:29:07 -0500

    Fix no app found.

--- runner_uwsgi.sh
+++ runner_uwsgi.sh
@@ -2,4 +2,4 @@
 
 #uwsgi --socket 127.0.0.1:8080 --wsgi-file server.py --callable app --processes 4 --threads 2   #use socket
 #uwsgi --http 127.0.0.1:8080 --wsgi-file server.py --callable app --processes 4 --threads 2    #use http
-uwsgi --ini ./uwsgi.ini
+uwsgi --ini ./uwsgi.ini --callable app
